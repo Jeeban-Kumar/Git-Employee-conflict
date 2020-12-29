56009f6d9cbba06552b81e59c76b442446a31c9a
Jeeban-Kumar <63390269+Jeeban-Kumar@users.noreply.github.com>
2020-12-29 22:25:24 +0530

    added two sout statements

--- 01-Employee-RestAPI/src/main/java/com/dash/Application.java
+++ 01-Employee-RestAPI/src/main/java/com/dash/Application.java
@@ -8,6 +8,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("Hello How are You ?");
+		System.out.println("You are very nice. How are You ?");
 	}
 
 }
